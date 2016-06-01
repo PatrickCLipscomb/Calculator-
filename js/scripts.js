121345fef5b2f86c3e5598f3f19394bb6b98a906
Kyle Helyar and Patrick Lipscomb <patrick@>
2016-06-01 11:28:10 -0700

    use js to create a form rather than a prompt to take user calculator input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+// Business Logic
+  var add = function(number1, number2) {
+    return number1 + number2;
+  };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+};
+// Interface Logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("input#add1").val());
+    var number2 = parseInt($("input#add2").val());
+    var result = add(number1, number2);
+    $('#output').text(result);
+  });
+});
